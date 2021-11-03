13e9626360adced30be6671aadb1ea86634a59ad
demirose41 <demirose41@gmail.com>
2021-11-02 17:42:12 -0700

    3. Built Wrapper Components utilizing props.child

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,9 +1,10 @@
-
+import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
     return (
      <li className={classes.item}>
+         <Card>
         <div className={classes.image}>
             <img src={props.image} alt={props.title}></img>
         </div>
@@ -15,6 +16,7 @@ function MeetupItem(props) {
         <div className={classes.actions}>
             <button>To Favorites</button>
         </div>
+        </Card>
     </li>
  );
 }
