2c35fc18b0702e64b2d421589e8706dcd44f3f65
demirose41 <demirose41@gmail.com>
2021-11-04 21:53:50 -0700

    7.Added Favorites page that displays current favorites saved through context

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,25 @@
+import { useContext } from 'react';
+
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetuoList';
+
 function FavoritesPage(){
-    return <div>Favorites page</div>
+    const favoritesCTX =  useContext(FavoritesContext)
+
+    let content;
+    
+    if(favoritesCTX.totalFavorites === 0) {
+        content = <p>You got no favorites yet. Would you like to add some?</p>
+    }else{
+        content = <MeetupList meetups={favoritesCTX.favorites}/>
+    }
+
+    return (
+        <section>
+            <h1>Favorites</h1>
+            {content}
+        </section>
+    )
 }
 
 export default FavoritesPage;
